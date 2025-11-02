7e151966e907926ae54007928d87e3ffff93bd46
GMK <mk4400320@gmail.com>
2025-11-02 18:03:12 +0530

    removed line

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -131,7 +131,7 @@ const Navbar: React.FC<{ toggleTheme?: () => void; darkMode?: boolean }> = ({
   const navigate = useNavigate();
 
   const handleLogout = () => {
-  dispatch(logout()); // clears auth slice
+  dispatch(logout()); 
   localStorage.removeItem("jwtToken"); // remove token
   sessionStorage.clear(); // clear any session cache if used
   navigate("/", { replace: true }); // redirect to landing page
