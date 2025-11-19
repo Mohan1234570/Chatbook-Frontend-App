e1a6f85cd30054655aecc4495a5e4e589dca61fd
GMK <mk4400320@gmail.com>
2025-11-19 22:02:30 +0530

    cleaned some code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,4 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import authReducer from './slices/authSlice';
-// import blogReducer from './slices/blogSlice';
 
-// export const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//     blog: blogReducer,
-//   },
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch; 
 
 
 import { configureStore } from '@reduxjs/toolkit';
